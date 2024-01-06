885bc5ad2bf317e99997809de7c710ae067cd2db
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-06 06:13:09 -0800

    Create project configuration

---
+++ config/app.php
@@ -0,0 +1,20 @@
+<?php
+
+define('APP_TITLE', 'mvc project');
+define('BASE_URL', 'http://localhost:8000');
+define('BASE_DIR', realpath(__DIR__ . "/../"));
+
+
+$temporary = str_replace(BASE_URL, "", explode("?", $_SERVER["REQUEST_URI"][0]));
+$temporary === "/" ? $temporary = "" : substr($temporary, 1);
+define("CURRENT_ROUTE", $temporary);
+
+global $routes;
+
+$routes = [
+    "get" => [],
+    "post" => [],
+    "put" => [],
+    "delete" => []
+];
+
