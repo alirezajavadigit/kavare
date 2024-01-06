df852fb7116f812f3c0b4e89e85140a604aaeb8a
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-06 09:29:59 -0800

    Update api.php define example routes

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,8 @@
 <?php 
+
+use System\Router\Api\Route;
+
+//Example
+Route::get("home", "HomeController@index", "home");
+Route::get("create", "HomeController@create", "create");
+Route::post("store", "HomeController@store", "store");
