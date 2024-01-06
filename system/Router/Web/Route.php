04c880ea4f3004dbca692231220e5962a1171def
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-06 09:13:19 -0800

    Create Route Handler

---
+++ system/Router/Web/Route.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace System\Router\Web;
+
+class Route{
+    public static function get($url, $executeMethod, $name = null){
+        $executeMethod = explode("@", $executeMethod);
+        $class = $executeMethod[0];
+        $method = $executeMethod[1];
+        global $routes;
+        array_push($routes['get'], array('url' => trim($url, "/ "), "class" => $class, "method" => $method, "name" => $name));
+    }
+}
