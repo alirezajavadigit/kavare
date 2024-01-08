f73c288345b45eb684958881a7b794a96f109415
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-07 23:19:36 -0800

    Update HasQueryBuilder.php
    add Geter and Seter for sql

--- system/Database/Traits/HasQueryBuilder.php
+++ system/Database/Traits/HasQueryBuilder.php
@@ -2,3 +2,25 @@
 
 namespace System\Database\Traits;
 
+use System\Database\DBConnection\DBConnection;
+
+trait HasQueryBuilder{
+    private $sql = "";
+    protected $where = [];
+    protected $orderBy = [];
+    protected $limit = [];
+    protected $values = [];
+    protected $bindValues = [];
+
+    protected function setSql($query){
+        $this->sql = $query;
+    }
+    protected function getSql(){
+        return $this->sql;
+    }
+    protected function resetSql(){
+        $this->sql = "";
+    }
+
+    
+}
