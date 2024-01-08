040c76988ea348da628f7c5c5626852012db1373
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-07 20:27:36 -0800

    Fixtypo app.php
    fixed a typo bug

--- config/app.php
+++ config/app.php
@@ -5,8 +5,8 @@ define('BASE_URL', 'http://localhost:8000');
 define('BASE_DIR', realpath(__DIR__ . "/../"));
 
 
-$temporary = str_replace(BASE_URL, "", explode("?", $_SERVER["REQUEST_URI"][0]));
-$temporary === "/" ? $temporary = "" : substr($temporary, 1);
+$temporary = str_replace(BASE_URL, "", explode("?", $_SERVER["REQUEST_URI"])[0]);
+$temporary === "/" ? $temporary = "" : $temporary = substr($temporary, 1);
 define("CURRENT_ROUTE", $temporary);
 
 global $routes;
