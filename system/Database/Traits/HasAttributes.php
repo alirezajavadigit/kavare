67fb5d5e07a3c26a329d9e5283a6f1650944aa28
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-09 09:11:13 -0800

    Update HasAttributes.php
    add all pure methods next step completing methods

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -2,3 +2,25 @@
 
 namespace System\Database\Traits;
 
+trait HasAttributes
+{
+    private function registerAttribute($object, string $attribute, $value)
+    {
+    }
+
+    protected function arrayToAttribute(array $array, $object = null)
+    {
+    }
+    protected function arrayToOnjects(){
+
+    }
+    private function inHiddenAttribute(){
+
+    }
+
+    private function inCasteAttribute(){}
+
+    private function caseDecodeValue(){}
+    private function caseEncodeValue(){}
+    private function arrayToCasteEncodeValue(){}
+}
