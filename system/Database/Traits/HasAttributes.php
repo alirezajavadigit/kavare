a425aedd603bb8d9764541db95159c8405f9b88e
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-09 09:21:08 -0800

    Update HasAttributes.php
    update registerAttribute method

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -6,21 +6,30 @@ trait HasAttributes
 {
     private function registerAttribute($object, string $attribute, $value)
     {
+        $this->inCasteAttribute($attribute) == true ? $object->$attribute = $this->casteDcodeAttribute($attribute, $value) : $object->$attribute = $value;
     }
 
     protected function arrayToAttribute(array $array, $object = null)
     {
     }
-    protected function arrayToOnjects(){
-
+    protected function arrayToOnjects()
+    {
     }
-    private function inHiddenAttribute(){
-
+    private function inHiddenAttribute()
+    {
     }
 
-    private function inCasteAttribute(){}
+    private function inCasteAttribute()
+    {
+    }
 
-    private function caseDecodeValue(){}
-    private function caseEncodeValue(){}
-    private function arrayToCasteEncodeValue(){}
+    private function caseDecodeValue()
+    {
+    }
+    private function caseEncodeValue()
+    {
+    }
+    private function arrayToCasteEncodeValue()
+    {
+    }
 }
