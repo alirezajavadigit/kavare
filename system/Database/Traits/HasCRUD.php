55a2f1aeeb639007620ddecb59e1d1dde4ca4595
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-10 06:46:23 -0800

    Update HasCRUD.php
    Update saveMethod

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -18,6 +18,19 @@ trait HasCRUD
             $this->addValue($this->primaryKey, $this->{$this->primaryKey});
         }
         $this->executeQuery();
+        $this->resetQuery();
+        if (!isset($this->{$this->primaryKey})) {
+            $object = $this->findMethod(DBConnection::newInsertId());
+            $defaultVars = get_class_vars(get_called_class());
+            $allVars = get_object_vars($object);
+            $differentVars = array_diff(array_keys($allVars), array_keys($defaultVars));
+            foreach($differentVars as $attribute){
+                $this->inCastsAttributes($attribute) == true ? $this->registerAttribute($this, $attribute, $this->castEncodeValue($attribute, $object->$attribute)) : $this->registerAttribute($this, $attribute, $object->$attribute);
+            }
+        }
+        $this->resetQuery();
+        $this->setAllowedMethods(['update', 'delete', 'find']);
+        return $this;
     }
 
     protected function fill()
