8c8d551d630cc0fb3ab2bb4c907e023b8f3f6e8e
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-10 05:57:42 -0800

    Update HasCRUD.php
    Create fill method

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -2,3 +2,21 @@
 
 namespace System\Database\Traits;
 
+use System\Database\DBConnection\DBConnection;
+
+trait HasCRUD
+{
+
+    protected function fill()
+    {
+        $fillArray = array();
+        foreach ($this->fillable as $attribute) {
+            if (isset($this->$attribute)) {
+                array_push($fillArraym, $this->getAttributeName($attribute) . " = ?");
+                $this->inCastsAttribute($attribute) == true ? $this->addValue($attribute, $this->castEncodeValue($attribute, $this->$attribute)) : $this->addValue($attribute, $this->$attribute);
+            }
+        }
+        $fillString = implode(", ", $fillArray);
+        return $fillString;
+    }
+}
