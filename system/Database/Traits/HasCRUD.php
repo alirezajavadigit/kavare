5699fe265f968de53bbb6aa801a8b8fa685958f1
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-10 06:09:02 -0800

    Update HasCRUD.php

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -7,6 +7,19 @@ use System\Database\DBConnection\DBConnection;
 trait HasCRUD
 {
 
+    protected function saveMethod()
+    {
+        $fillString = $this->fill();
+        if (!isset($this->{$this->primaryKey})) {
+            $this->setSql("INSERT INTO " . $this->getTableName() . " SET " . $fillString . " , " . $this->getAttributeName($this->createdAt) . " = NOW()");
+        } else {
+            $this->setSql("UPDATE INTO " . $this->getTableName() . " SET " . $fillString . " , " . $this->getAttributeName($this->updatedAt) . " = NOW()");
+            $this->setWhere("AND", $this->getAttributeName($this->primaryKey) . " = ?");
+            $this->addValue($this->primaryKey, $this->{$this->primaryKey});
+        }
+        $this->executeQuery();
+    }
+
     protected function fill()
     {
         $fillArray = array();
