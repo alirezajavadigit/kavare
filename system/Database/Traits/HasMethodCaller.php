71d3dc9da9bd7967302911db775a25424c9846ab
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-19 06:24:50 -0800

    Update HasMethodCalller

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -2,3 +2,10 @@
 
 namespace System\Database\Traits;
 
+trait HasMethodCaller{
+    private $allMethod = ['create', 'update', 'delete', 'find', 'all', 'where', 'whereOr', 'save', 'whereIn', 'whereNull', 'whereNotNull', 'orderBy', 'get', 'limit', 'paginate'];
+    private $allowedMethod = ['create', 'update', 'delete', 'find', 'all', 'where', 'whereOr', 'save', 'whereIn', 'whereNull', 'whereNotNull', 'orderBy', 'get', 'limit', 'paginate'];
+    protected function setAllowedMethods($array){
+        $this->allowedMethod = $array;
+    }
+}
