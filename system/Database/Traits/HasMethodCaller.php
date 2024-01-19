ef7f49ea7e98e8661f5fdaa71740c9b8d87c97a5
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-19 07:04:34 -0800

    Update HasMethodCaller

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -2,10 +2,34 @@
 
 namespace System\Database\Traits;
 
-trait HasMethodCaller{
-    private $allMethod = ['create', 'update', 'delete', 'find', 'all', 'where', 'whereOr', 'save', 'whereIn', 'whereNull', 'whereNotNull', 'orderBy', 'get', 'limit', 'paginate'];
+trait HasMethodCaller
+{
+    private $allMethods = ['create', 'update', 'delete', 'find', 'all', 'where', 'whereOr', 'save', 'whereIn', 'whereNull', 'whereNotNull', 'orderBy', 'get', 'limit', 'paginate'];
     private $allowedMethod = ['create', 'update', 'delete', 'find', 'all', 'where', 'whereOr', 'save', 'whereIn', 'whereNull', 'whereNotNull', 'orderBy', 'get', 'limit', 'paginate'];
-    protected function setAllowedMethods($array){
+
+    public function __call($method, $args)
+    {
+        return $this->methodCaller($this, $method, $args);
+    }
+
+    public function callStatic($method, $args)
+    {
+        $className = get_called_class();
+        $instance = new $className;
+        return $this->methodCaller($instance, $method, $args);
+    }
+
+    protected function setAllowedMethods($array)
+    {
         $this->allowedMethod = $array;
+    }
+
+    private function methodCaller($object, $method, $args)
+    {
+        $suffix = "Method";
+        $methodName = $method . $suffix;
+        if (in_array($methodName, $this->allowedMethod)) {
+            return call_user_func_array(array($object, $methodName), $args);
+        }
     }
 }
