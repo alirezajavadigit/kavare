f3fa32527b3d7ce378960fb0ce57b4077e022a6e
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-19 05:38:38 -0800

    Update HasCRUD
    Fixed a typo

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -164,7 +164,7 @@ trait HasCRUD
         $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
         $totalPages = ceil($totalRows / $perPage);
         $currentPage = min($currentPage, $totalPages);
-        $currentPage = max($currentPage, $totalPages);
+        $currentPage = max($currentPage, 1);
         $currentRow = ($currentPage - 1) * $perPage;
         $this->setLimit($currentRow, $perPage);
         if ($this->sql == "") {
