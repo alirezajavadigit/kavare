8fcbede05c5d0169be3050c13978d987eb826367
alirezajawadi <alirezajavadigit@gmail.com>
2024-01-19 08:57:19 -0800

    Update HasSoftDelete
    Create deleteMethod

--- system/Database/Traits/HasSoftDelete.php
+++ system/Database/Traits/HasSoftDelete.php
@@ -2,3 +2,21 @@
 
 namespace System\Database\Traits;
 
+trait HasSoftDelete
+{
+    protected function deleteMethod($id = null)
+    {
+        $object = $this;
+        if ($id) {
+            $this->resetQuery();
+            $object = $this->findMethod($id);
+        }
+        if ($object) {
+            $object->resetQuery();
+            $object->setSql("UPDATE  " . $object->getTableName() . " SET " . $this->getAttributeName($this->deletedAt) . " = NOW()");
+            $object->setWhere("AND", $this->getAttributeName($object->primaryKey) . " = ?");
+            $object->addValue($object->primaryKey, $object->{$object->primartyKey});
+            return $object->executeQuery();
+        }
+    }
+}
