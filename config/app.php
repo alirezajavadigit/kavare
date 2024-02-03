cd523b1fed2ee88cf6f620b23f69e0298f28a0e4
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-03 04:14:49 -0800

    CleanUp app.php

--- config/app.php
+++ config/app.php
@@ -1,12 +1,14 @@
 <?php
-
 define('APP_TITLE', 'mvc project');
+
 define('BASE_URL', 'http://localhost:8000');
+
 define('BASE_DIR', realpath(__DIR__ . "/../"));
 
-
 $temporary = str_replace(BASE_URL, "", explode("?", $_SERVER["REQUEST_URI"])[0]);
+
 $temporary === "/" ? $temporary = "" : $temporary = substr($temporary, 1);
+
 define("CURRENT_ROUTE", $temporary);
 
 global $routes;
@@ -17,4 +19,3 @@ $routes = [
     "put" => [],
     "delete" => []
 ];
-
