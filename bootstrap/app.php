7f350ca69aaa91cf422f97532e0ae9e45d2a878e
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-03 03:55:07 -0800

    Update app.php
    update app.php and adding dd helper function and fixed typo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,17 @@
-<?php 
+<?php
+// dd helper function
+function dd($var)
+{
+    echo '<pre>';
+    print_r($var);
+    exit;
+}
 
-require_once("../config/app.php");
-require_once("../config/database.php");
+require_once(__DIR__ . "/../config/app.php");
+require_once(__DIR__ . "/../config/database.php");
 
-require_once("../routes/web.php");
-require_once("../routes/api.php");
+require_once(__DIR__ . "/../routes/web.php");
+require_once(__DIR__ . "/../routes/api.php");
 
 $routing = new \System\Router\Routing();
 $routing->run();
