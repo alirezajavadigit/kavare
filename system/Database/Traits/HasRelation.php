8f1efde3d187bfb6e9c5c56212f774ceed93a4b9
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-02 21:35:32 -0800

    Update HasRelation
    create belongsToMany

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -66,4 +66,21 @@ trait HasRelation
             return $this->arrayToAttributes($data);
         return null;
     }
+
+    protected function belongsToMany($model, $commonTable, $localKey, $middleForeignKey, $middleRelation, $foreignKey)
+    {
+        if ($this->{$this->primaryKey}) {
+            $modelObject = new $model();
+            return $modelObject->getBelongsToManyRelation($this->table, $commonTable, $localKey, $this->$localKey, $middleForeignKey, $middleRelation, $foreignKey);
+        }
+    }
+
+    protected function getBelongsToManyRelation($table, $commonTable, $localKey, $localKeyValue, $middleForeignKey, $middleRelation, $foreignKey)
+    {
+        // $sql = "SELECT posts.* FROM ( SELECT category_post.* FROM categories JOIN category_post on categories.id = category_post.cat_id WHERE  categories.id = ? ) as relation JOIN posts on relation.post_id=posts.id ";
+        $this->setSql("SELECT `c`.* FROM ( SELECT `b`.* FROM `{$table}` AS `a` JOIN `{$commonTable}` AS `b` on `a`.`{$localKey}` = `b`.`{$middleForeignKey}` WHERE  `a`.`{$localKey}` = ? ) AS `relation` JOIN " . $this->getTableName() . " AS `c` ON `relation`.`{$middleRelation}` = `c`.`$foreignKey`");
+        $this->addValue("{$table}_{$localKey}", $localKeyValue);
+        $this->table = 'c';
+        return $this;
+    }
 }
