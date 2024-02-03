c0d79aa640d0e10422e3b3b08a6d004481404bc5
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-02 21:23:38 -0800

    Update HasRelation
    create hasMany method

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -25,4 +25,23 @@ trait HasRelation
             return $this->arrayToAttributes($data);
         return null;
     }
+
+    protected function hasMany($model, $foreignKey, $otherKey)
+    {
+        if ($this->{$this->primaryKey}) {
+            $modelObject = new $model;
+            return $modelObject->getHasManyRelation($this->table, $foreignKey, $otherKey, $this->$otherKey);
+        }
+    }
+
+    public function getHasManyRelation($table, $foreignKey, $otherKey, $otherKeyValue)
+    {
+        // sql = 'SELECT posts.* FROM categories JOIN posts ON categories.id = posts.cat_id'
+        // sql = 'SELECT categories.* FROM categories JOIN categories ON categories.id = categories.parent_id'
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN " . $this->getTableName() . " AS `b` on `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+        $this->setWhere('AND', "`a`.`$otherKey` = ? ");
+        $this->table = 'b';
+        $this->addValue($otherKey, $otherKeyValue);
+        return $this;
+    }
 }
