7e57525b6616fdb24d13fdc639a3a97f76cf0304
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-02 21:16:02 -0800

    Create HasRelation
    create hasOne method

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -2,3 +2,27 @@
 
 namespace System\Database\Traits;
 
+trait HasRelation
+{
+    protected function hasOne($model, $foreignKey, $localKey)
+    {
+        if ($this->{$this->primaryKey}) {
+            $modelObject = new $model();
+            return $modelObject->getHasOneRelation($this->table, $foreignKey, $localKey, $this->$localKey);
+        }
+    }
+
+    public function getHasOneRelation($table, $foreignKey, $otherKey, $otherKeyValue)
+    {
+        // sql = 'SELECT phones.* FROM users JOIN phones ON users.id = phones.user_id'
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN " . $this->getTableName() . " AS `b` on `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+        $this->setWhere('AND', "`a`.`$otherKey` = ? ");
+        $this->table = 'b';
+        $this->addValue($otherKey, $otherKeyValue);
+        $statement = $this->executeQuery();
+        $data = $statement->fetch();
+        if ($data)
+            return $this->arrayToAttributes($data);
+        return null;
+    }
+}
