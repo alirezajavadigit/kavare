6acf3c8ef620980588a62f7c5247580e8d3098ea
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-03 04:01:59 -0800

    Renew DBConnection.php
    renew dbconnection.php after test

--- system/Database/DBConnection/DBConnection.php
+++ system/Database/DBConnection/DBConnection.php
@@ -1,34 +1,47 @@
 <?php
 
+
 namespace System\Database\DBConnection;
 
 use PDO;
 use PDOException;
 
-class DBConnection{
+class DBConnection
+{
+
     private static $dbConnectionInstance = null;
 
-    private function __construct(){}
+    private function __construct()
+    {
+    }
 
-    public static function getDBConnectionInstance(){
-        if(self::$dbConnectionInstance == null){
+    public static function getDBConnectionInstance()
+    {
+
+        if (self::$dbConnectionInstance == null) {
             $DBConnectionInstance = new DBConnection();
             self::$dbConnectionInstance = $DBConnectionInstance->dbConnection();
         }
+
         return self::$dbConnectionInstance;
     }
 
-    private function dbConnection(){
-        try{
-            $options = array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
-            $dbConnection = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME.";charset=utf8", DBUSERNAME, DBPASSWORD, $options);
-            return $dbConnection;
-        }catch(PDOException $e){
-            echo "Error in database connection: " .$e->getMessage();
+    private function dbConnection()
+    {
+
+        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+        try {
+            return new PDO("mysql:host=" . DBHOST . ";dbname=" . DBNAME, DBUSERNAME, DBPASSWORD, $options);
+        } catch (PDOException $e) {
+            echo "error in database connection: " . $e->getMessage();
+            return false;
         }
     }
 
-    public static function newInsertId(){
+
+    public static function newInsertId()
+    {
+
         return self::getDBConnectionInstance()->lastInsertId();
     }
 }
