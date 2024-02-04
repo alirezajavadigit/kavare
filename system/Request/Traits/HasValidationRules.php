77d308b68bb9f299ddff5583103556c8fcbc9232
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-04 07:21:07 -0800

    Update HasValidationRules.php

--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -89,6 +89,39 @@ trait HasValidationRules
         }
     }
 
+    protected function required($name)
+    {
+        if ((!isset($this->request[$name]) || $this->request[$name] === '') && $this->checkFirstError($name)) {
+            $this->setError($name, "$name is required");
+        }
+    }
+
+    protected function number($name)
+    {
+        if ($this->checkFieldExist($name)) {
+            if (!is_numeric($this->request[$name]) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be a number");
+            }
+        }
+    }
+
+    protected function date($name)
+    {
+        if ($this->checkFieldExist($name)) {
+            $pattern = '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]|[0-9]|3[0-1])$/';
+            if (!preg_match($pattern, $this->request[$name]) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be date format");
+            }
+        }
+    }
+
+    protected function email($name){
+        if ($this->checkFieldExist($name)) {
+            if (!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be a valid email");
+            }
+        }
+    }
 
     protected function checkFieldExist($name)
     {
