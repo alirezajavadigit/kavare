5b9f89aff5e4549a74e315a79be439c6257e9e03
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-07 00:51:27 -0800

    Update ViewBuilder.php

--- system/View/ViewBuilder.php
+++ system/View/ViewBuilder.php
@@ -2,20 +2,27 @@
 
 namespace System\View;
 
+use App\Providers\AppServiceProvider;
 use System\View\Traits\HasViewLoader;
 use System\View\Traits\HasExtendsContent;
 use System\View\Traits\HasIncludeContent;
+use Exception;
 
 class ViewBuilder
 {
     use HasViewLoader, HasExtendsContent, HasIncludeContent;
 
     public $content;
+    public $vars = [];
 
     public function run($dir)
     {
         $this->content = $this->viewLoader($dir);
         $this->checkExtendsContent();
         $this->checkIncludesContent();
+        Composer::setViews($this->viewNameArray);
+        $appServiceProvider = new AppServiceProvider();
+        $appServiceProvider->boot();
+        $this->vars = Composer::getVars();
     }
 }
