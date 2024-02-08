72d864c148585ee2c01b8e43a0894d6a0a1186a4
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-08 00:05:44 -0800

    Update app.php
    prepare for old helper

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,18 @@
 <?php
+session_start();
+
+if (isset($_SESSION['old'])) unset($_SESSION['temporary_old']);
+
+if (isset($_SESSION['old'])) {
+    $_SESSION['temporary_old'] = $_SESSION['old'];
+    unset($_SESSION['old']);
+}
+
+$params = [];
+$params = !isset($_GET) ? $params : array_merge($params, $_GET);
+$params = !isset($_POST) ? $params : array_merge($params, $_POST);
+$_SESSION['old'] = $params;
+unset($params);
 
 require_once(__DIR__ . "/../system/helpers/helpers.php");
 require_once(__DIR__ . "/../config/app.php");
