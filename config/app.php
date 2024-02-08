7add62afc6ab42062c4be3bfe4b06eb5fce443b3
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-08 01:16:19 -0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -3,5 +3,9 @@
 return [
     'APP_TITLE' => "Kavare",
     "BASE_URL" => "http://localhost:8000",
-    "BASE_DIR" => dirname(__DIR__)
+    "BASE_DIR" => dirname(__DIR__),
+    'providers' => [
+        \App\Providers\SessionProivder::class,
+        \App\Providers\AppServiceProvider::class,
+    ]
 ];
