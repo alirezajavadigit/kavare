6021fb66590f8d0afd6675934070ffd1dd94ef5b
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-08 00:06:25 -0800

    Update helpers.php
    add dd and html and old helpers

--- system/helpers/helpers.php
+++ system/helpers/helpers.php
@@ -1,11 +1,35 @@
 <?php
 
-function view($dir, $vars = []){
+function dd($value, $die = true)
+{
+    echo '<pre>';
+    print_r($value);
+    if ($die) {
+        exit;
+    }
+}
+
+function view($dir, $vars = [])
+{
     $viewBuilder = new \System\View\ViewBuilder();
     $viewBuilder->run($dir);
     $viewVars = $viewBuilder->vars;
     $content = $viewBuilder->content;
-    empty($viewVars) ? : extract($viewVars);
-    empty($vars) ? : extract($vars);
+    empty($viewVars) ?: extract($viewVars);
+    empty($vars) ?: extract($vars);
     eval(" ?>" . html_entity_decode($content));
+}
+
+function html($text)
+{
+    return html_entity_decode($text);
+}
+
+function old($name)
+{
+    if (isset($_SESSION['temporary_old'][$name])) {
+        return $_SESSION['temporary_old'][$name];
+    } else {
+        return null;
+    }
 }
