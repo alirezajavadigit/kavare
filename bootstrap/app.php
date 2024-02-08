deacf5b70d240ea3864a0e700628e92ee2f8870b
alirezajawadi <alirezajavadigit@gmail.com>
2024-02-08 00:16:04 -0800

    Update app.php
    prepare app.php for errorflash and flash helpers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,21 @@
 <?php
 session_start();
 
+if (isset($_SESSION['temporary_errorFlash'])) unset($_SESSION['temporary_errorFlash']);
+if (isset($_SESSION['temporary_flash'])) unset($_SESSION['temporary_flash']);
 if (isset($_SESSION['old'])) unset($_SESSION['temporary_old']);
 
 if (isset($_SESSION['old'])) {
     $_SESSION['temporary_old'] = $_SESSION['old'];
     unset($_SESSION['old']);
+}
+if (isset($_SESSION['flash'])) {
+    $_SESSION['temporary_flash'] = $_SESSION['flash'];
+    unset($_SESSION['flash']);
+}
+if (isset($_SESSION['errorFlash'])) {
+    $_SESSION['temporary_errorFlash'] = $_SESSION['errorFlash'];
+    unset($_SESSION['errorFlash']);
 }
 
 $params = [];
